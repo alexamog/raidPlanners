8c897be5497442e11bdbfe5d4fe474a4f9e41914
jprili <rilijames@gmail.com>
2023-02-16 18:58:34 -0800

    (untested) implemented getter by ID

--- raid-backend/controller/hangController.js
+++ raid-backend/controller/hangController.js
@@ -5,12 +5,29 @@ const conn  = mysql.createConnection({
     database: 'raidDB'
 });
 
-/*
+const Hangout = (title, sDT, eDT, desc, loc) => {
+    this.title = title;
+    this.sDT   =   sDT;
+    this.eDT   =   eDT;
+    this.desc  =  desc;
+    this.loc   =   loc;
+}
+
+/**
  *  DOM-inspired setters and getters
  */
 
-const getHangoutByID = (id) => {
-    return null;
+/** 
+ * gets a hangout object by id
+ * 
+ * @param {string} id 
+ * @returns {Hangout} A hangout object of the matching ID
+ */
+const getHangoutByID = async (id) =>  {
+    const [rows, fields] = await conn.execute("select * from `hangouts` where `id` = ?", [id])
+    const result = Hangout(fields);
+    
+    return result;
 };
 
 const getHangoutsByTitle = (title) =>  {
