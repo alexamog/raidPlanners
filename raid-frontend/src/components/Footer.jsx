67c255c50f0866b38b04f3f9fb7f7f1db4115899
root <55458737+BryanCL01@users.noreply.github.com>
2023-02-16 17:08:12 -0800

    Update Footer.jsx

--- raid-frontend/src/components/Footer.jsx
+++ raid-frontend/src/components/Footer.jsx
@@ -9,7 +9,8 @@ import {
 
 export default function Footer() {
     return (
-        <Box 
+        <Box
+            mt={'1em'}
             bg={useColorModeValue('gray.50', 'gray.900')}
             color={useColorModeValue('gray.700', 'gray.200')}>
             <Container
@@ -18,8 +19,8 @@ export default function Footer() {
                 py={10}
                 direction={{ base: 'column', md: 'row' }}
                 spacing={4}
-                justify={{ base: 'center', md: 'space-between' }}
-                align={{ base: 'center', md: 'center' }}>
+                justify={{ md: 'space-between' }}
+                align={{md: 'center' }}>
                 <Text>Raid Planner 2023 | Made with ❤️</Text>
             </Container>
         </Box>
