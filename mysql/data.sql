90ae7eab658bbfa6c98cea64f6f82c395503b7f5
Alex Amog <76136244+alexamog@users.noreply.github.com>
2023-03-21 22:52:42 -0700

    Update data.sql

--- mysql/data.sql
+++ mysql/data.sql
@@ -7,7 +7,7 @@ USE raid;
 CREATE TABLE users(
     user_id VARCHAR(255) NOT NULL,
     user_name VARCHAR(255) NOT NULL,
-    user_avatar VARCHAR(255) NOT NULL,
+    user_avatar VARCHAR(255),
     user_discriminator VARCHAR(5) NOT NULL,
     PRIMARY KEY (user_id)
 );
@@ -32,10 +32,6 @@ CREATE TABLE attendees(
     FOREIGN KEY (hangout_id) REFERENCES hangouts(hangout_id)
 );
 
-INSERT INTO users(user_id,user_name,user_avatar,user_discriminator) VALUES('100319324333432832', 'Seisname','a57b03dcb179eb2ca827f55fbb828b08','8162');
-INSERT INTO hangouts(user_id, hangout_title, hangout_description, hangout_date, hangout_location) VALUES("100319324333432832", "Raid Hangout", "Lets raid and game", "2023-02-25T13:00", "My place!");
-
-INSERT INTO attendees(user_id, hangout_id) VALUES("100319324333432832", 1);
 CREATE USER 'raid-user' @'%' IDENTIFIED BY 'raiduser-0000';
 
 GRANT ALL PRIVILEGES ON raid.* TO 'raid-user' @'%' WITH GRANT OPTION;
