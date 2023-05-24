407e626b2fd987eff2275db32370c6aa7983c1b1
Alex A <alexamog@outlook.com>
2023-05-24 12:24:51 -0700

    Added event bot

---
+++ event-bot/app.py
@@ -0,0 +1,31 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+
+import asyncio
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(name='RaidPlanners', type=0))
+    print(discord.__version__)
+    print(f"{bot.user.name} - {bot.user.id}")
+    print('Up and running')
+
+
+async def load_extensions():
+    await bot.load_extension(f"cogs.commands")
+    await bot.load_extension(f"cogs.automation")
+
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
